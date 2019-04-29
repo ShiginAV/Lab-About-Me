54b4308e8d3625c93c721e071651af3212531ed6
Aleksander Shigin <a.sh.10031987@gmail.com>
2019-04-29 19:59:49 +0300

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Alexander on 29/04/2019.
+//  Copyright © 2019 Alexander Shigin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
